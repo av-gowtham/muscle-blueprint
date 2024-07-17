24820b5a04d329540125c1238e76c1e59e95fc4c
av-gowtham <gowthamavg10@gmail.com>
2024-07-17 18:36:51 +0530

    changed the name

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -6,7 +6,7 @@ function Head() {
       <div className="flex flex-col gap-4">
         <p>IT'S TIME TO GET</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-          swole<span className="text-blue-400">Normous</span>
+          Muscle<span className="text-blue-400">Blueprint</span>
         </h1>
       </div>
       <p className="text-sm md:text-base font-light">
